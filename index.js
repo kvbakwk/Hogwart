e87da5105a0fb5fe303472b8cab6487170939c3c
Jakub Kawka <jakubkawka2005@gmail.com>
2023-10-27 13:55:03 +0200

    done

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
-const imageEl = document.querySelector("img");
+const pElements = document.querySelectorAll("p");
+const button = document.querySelector("button");
 
-document.querySelector("#mountains").onchange = (e) => {
-  switch (parseInt(e.target.value)) {
-    case 1:
-      console.log("s");
-      imageEl.src = "./img/tatry.jpg";
-      imageEl.style.display = "block";
-      break;
-    case 2:
-      imageEl.src = "./img/karkonosze.jpg";
-      imageEl.style.display = "block";
-      break;
-    case 3:
-      imageEl.src = "./img/beskidy.jpg";
-      imageEl.style.display = "block";
-      break;
-    default:
-      imageEl.src = "";
-      imageEl.style.display = "none";
-      break;
-  }
-};
+const colors = ["red", "green", "yellow"];
+
+pElements.forEach((pElement) => {
+  pElement.style.width = "200px";
+  pElement.style.height = "200px";
+  pElement.style.backgroundColor = "gray";
+});
+
+button.onclick = (e) =>
+  pElements.forEach(
+    (pElement) =>
+      (pElement.style.backgroundColor = colors[Math.floor(Math.random() * 3)])
+  );
