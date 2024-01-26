1a586e6da1687e9583c055f334ecb8533e7880ca
Jakub Kawka <jakubkawka2005@gmail.com>
2024-01-26 09:51:45 +0100

    one error

---
+++ script.js
@@ -0,0 +1,27 @@
+document.querySelector("select").onchange = (e) =>
+  (document.querySelector(".right").style.color = e.target.value);
+document.querySelector("#fontSize").onblur = (e) =>
+  (document.querySelector(".right").style.fontSize = e.target.value);
+document.querySelector("#border").onchange = (e) => {
+  console.log(e.target);
+  e.target.checked
+    ? (document.querySelector("img").style.borderWidth = "1px")
+    : (document.querySelector("img").style.borderWidth = "0px");
+};
+document
+  .querySelectorAll("button")
+  .forEach(
+    (bttn) =>
+      (bttn.onclick = () =>
+        (document.querySelector(".right").style.backgroundColor =
+          bttn.textContent))
+  );
+document
+  .querySelectorAll("input")
+  .forEach(
+    (i) =>
+      (i.onchange = (e) =>
+        i.type === "radio" &&
+        e.target.checked &&
+        (document.querySelector("ul").style.listStyleType = e.target.id))
+  );
